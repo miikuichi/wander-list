8a30ed4ed32100dc6f3d7102d048ed9fdc8fcccf
Christian Kyle Tapales <tapaleskyle629@gmail.com>
2025-10-07 23:48:08 +0800

    Feat: Connected the login to the dashboard and give the logout button in the dashboard a functionality

--- wander_list/core/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-@login_required
-def home_view(request):
-    return render(request, 'core/home.html')
--- wander_list/login/urls.py
+++ wander_list/login/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     # This maps the URL path 'login/' to the 'login_view' function
     path('', views.login_view, name='login_page'),
     path('register/', views.register, name='register_page'),
+    #logout functionality
+    path('logout/', views.logout_view, name='logout'), 
 ]
