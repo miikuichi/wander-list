42d242c0317915da102a31e2eb5a8f491f997f79
LAB23_USER ON WS42 <L23X16W42@LabHCI.net>
2025-10-29 17:45:00 +0800

    added allowed hosts

--- wander_list/settings.py
+++ wander_list/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = 'django-insecure-0n-y*q!%qlk=#d+2_uw21zmj#m_wlxxxbx25wmzen+vc$@3muq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://pisoheroes.onrender.com/']
 
 
 # Application definition
